4a64d4ac710274bea02facd468414822ace77421
NERTIL <Nertilpoci@hotmail.com>
2017-08-27 23:44:12 +0200

    Implemented output monitor

---
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteredOutputWindowVSX
+{
+  public static class Helpers
+    {
+        public static WritableSettingsStore GetWritableSettingsStore(this SVsServiceProvider vsServiceProvider)
+        {
+            var shellSettingsManager = new ShellSettingsManager(vsServiceProvider);
+            return shellSettingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+        }
+    }
+}
