9ad6d40ca90c69df0e14a9908f0800f59a48d137
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-18 21:02:42 +0200

    Add simple relay command

--- FilteredOutputWindowVSX/FilteredOutputWindowVSX/Tools/Helpers.cs
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Tools/Helpers.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Settings;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Tools/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FilteredOutputWindowVSX.Tools
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute = null;
+        private readonly Func<bool> _canExecute = null;
+
+        public RelayCommand(Action a_execute)
+            : this(a_execute, null)
+        {
+        }
+
+        public RelayCommand(Action a_execute, Func<bool> a_canExecute)
+        {
+            _execute = a_execute ?? throw new ArgumentNullException(nameof(a_execute));
+            _canExecute = a_canExecute;
+        }
+
+        public bool CanExecute(object a_parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object a_parameter)
+        {
+            _execute();
+        }
+    }
+}
