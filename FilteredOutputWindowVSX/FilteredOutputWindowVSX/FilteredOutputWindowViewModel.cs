f74844e3b06422d7dc9fcebec4df6b9138e08b47
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-18 21:11:53 +0200

    update View model

--- FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowViewModel.cs
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowViewModel.cs
@@ -1,29 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using EnvDTE;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio;
 using System.Windows.Input;
+using System.ComponentModel;
+using FilteredOutputWindowVSX.Tools;
+using System.Runtime.CompilerServices;
 
 namespace FilteredOutputWindowVSX
 {
     public class FilteredOutputWindowViewModel : INotifyPropertyChanged
     {
+        private DTE _dte;
+        private Events _dteEvents;
+        private OutputWindowEvents _documentEvents;
+
         public FilteredOutputWindowViewModel()
         {
+            SetupEvents();
+            CreateCommands();
 
+            Tags = Properties.Settings.Default.Tags ?? "";
         }
 
-        public string Output { get; set; }
-        public string Tags { get; set; }
-        public bool AutoScroll { get; set; }
+        private void SetupEvents()
+        {
+            _dte = (DTE)Package.GetGlobalService(typeof(SDTE));
+            _dteEvents = _dte.Events;
+            _documentEvents = _dteEvents.OutputWindowEvents;
+        }
 
-        public ICommand StartRecording { get; set; }
-        public ICommand StopRecording { get; set; }
-        public ICommand Clear { get; set; }
+        #region Prop for ViewModel
+        private string _output;
+        private string _tags;
+        private bool _autoScroll;
 
+        public string Output
+        {
+            get => _output;
+            set
+            {
+                if (_output == value) return;
+                _output = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Tags
+        {
+            get => _tags;
+            set
+            {
+                if (_tags == value) return;
+
+                _tags = value;
+                NotifyPropertyChanged();
+
+                Properties.Settings.Default.Tags = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        public bool AutoScroll
+        {
+            get => _autoScroll;
+            set
+            {
+                if (_autoScroll == value) return;
+                _autoScroll = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand StartRecording { get; private set; }
+        public ICommand StopRecording { get; private set; }
+        public ICommand Clear { get; private set; } 
+        #endregion
+
+        private void CreateCommands()
+        {
+            StartRecording = new RelayCommand(() =>
+            {
+
+            });
+
+            StopRecording = new RelayCommand(() =>
+            {
+
+            });
+
+            Clear = new RelayCommand(() =>
+            {
+
+            });
+        }
 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
--- FilteredOutputWindowVSX/FilteredOutputWindowVSX/Tools/Helpers.cs
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Tools/Helpers.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Settings;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FilteredOutputWindowVSX.Tools
 {
