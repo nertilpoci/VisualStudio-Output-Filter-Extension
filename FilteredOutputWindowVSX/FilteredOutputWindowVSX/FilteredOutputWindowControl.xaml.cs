f6c75d477ab86888a60a3a6a44bd6906489bc486
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-21 21:10:04 +0200

    remove old comments

--- FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowControl.xaml.cs
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowControl.xaml.cs
@@ -10,14 +10,8 @@
     using System.Linq;
     using Microsoft.VisualStudio;
 
-    /// <summary>
-    /// Interaction logic for FilteredOutputWindowControl.
-    /// </summary>
     public partial class FilteredOutputWindowControl : UserControl
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FilteredOutputWindowControl"/> class.
-        /// </summary>
         public FilteredOutputWindowControl()
         {
             this.InitializeComponent();
@@ -28,57 +22,5 @@
                     Scroller.ScrollToEnd();
             };
         }
-
-        //private void _documentEvents_PaneUpdated(OutputWindowPane pPane)
-        //{
-        //    if (string.IsNullOrEmpty(tagsBox.Text)) return;
-        //    pPane.TextDocument.Selection.SelectAll();
-        //    if (pPane.Name == "Debug")
-        //    {
-        //        try
-        //        {
-        //            var name = pPane.Name;
-        //            TextDocument doc = pPane.TextDocument;
-        //            TextSelection sel = doc.Selection;
-        //            sel.StartOfDocument(false);
-        //            sel.EndOfDocument(true);
-        //            outputBox.Document.Blocks.Clear();
-        //            var tags = tagsBox.Text.TrimStart().Split(',');
-        //            var textLines = sel.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-        //            foreach (var line in textLines)
-        //            {
-        //                foreach (var tag in tags)
-        //                {
-        //                    if (line.StartsWith(tag))
-        //                    {
-        //                        outputBox.AppendText(line.Replace(tag.TrimStart(), ""));
-        //                        outputBox.AppendText(Environment.NewLine);
-        //                    }
-
-        //                }
-        //                if (tags.Any(t => line.StartsWith(t))){
-        //                    ;
-        //                }
-        //            }                               
-        //        }
-        //        catch (Exception ex)
-        //        {
-
-
-        //        }
-        //        if(ScrollToEndCheck.IsChecked??false) outputBox.ScrollToEnd();
-
-        //    }
-        //}
-
-        //private void CleanButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    IVsOutputWindow outWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
-        //    Guid debugPaneGuid = VSConstants.GUID_OutWindowDebugPane;
-        //    IVsOutputWindowPane pane;
-        //    outWindow.GetPane(ref debugPaneGuid, out pane);
-        //    pane.Clear();
-        //    outputBox.Document.Blocks.Clear();
-        //}
     }
 }
