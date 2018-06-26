e2f561ddeb1138d20813788f3969045374dc1dc1
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-26 22:52:05 +0200

    fix pictures

---
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Converters/PictureConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FilteredOutputWindowVSX.Converters
+{
+    public class PictureConverter : IValueConverter
+    {
+        private const string ResFolder = "Resources";
+
+        public static bool IsInDesign = DesignerProperties.GetIsInDesignMode(new DependencyObject());
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is string input)
+            {
+#if DEBUG
+                // Note -> do not deploy debug!
+                if (IsInDesign)
+                {
+                    return Path.Combine(ResFolder, input);
+                }
+#endif
+                return GetImageFullPath(input);
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public string GetImageFullPath(string filename)
+        {
+            var folder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            return Path.Combine(folder, ResFolder, filename);
+        }
+    }
+}
---
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/ViewModels/Design/DesignStringFilterContainer.cs
@@ -0,0 +1,36 @@
+﻿using FilteredOutputWindowVSX.Models;
+
+namespace FilteredOutputWindowVSX.ViewModels.Design
+{
+    public class DesignStringFilterContainer : StringFilterContainer
+    {
+        public DesignStringFilterContainer()
+        {
+            Name = "Super filter";
+
+            Filter = new StringFilterItem
+            {
+                Operation = Enums.StringOperation.Contains,
+                Value = "#a filter"
+            };
+
+        }
+
+        public StringFilterContainer EditingFilter
+        {
+            get
+            {
+                return new StringFilterContainer()
+                {
+                    Name = "Super filter",
+
+                    Filter = new StringFilterItem
+                    {
+                        Operation = Enums.StringOperation.Contains,
+                        Value = "#a filter"
+                    }
+                };
+            }
+        }
+    }
+}
