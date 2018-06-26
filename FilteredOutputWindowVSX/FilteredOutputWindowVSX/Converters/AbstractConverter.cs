1c15d5ac6e53de524bf1dde3591c00a299337b3c
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-06-26 22:59:40 +0200

    Add abstract converter

---
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/Converters/AbstractConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FilteredOutputWindowVSX.Converters
+{
+    public abstract class AbstractConverter<T> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool input && input)
+            {
+                return Positive;
+            }
+            return Negative;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public abstract T Positive { get; set; }
+
+        public abstract T Negative { get; set; }
+    }
+}
