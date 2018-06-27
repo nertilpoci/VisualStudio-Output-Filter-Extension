2a4d27f2356eb5c851133bd8d09c7b8aa567c15e
Nertil Poci <nertilpoci@hotmail.com>
2018-06-27 10:32:00 -0700

    update

--- FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowControl.xaml.cs
+++ FilteredOutputWindowVSX/FilteredOutputWindowVSX/FilteredOutputWindowControl.xaml.cs
@@ -12,10 +12,15 @@
 
     public partial class FilteredOutputWindowControl : UserControl
     {
+      
         public FilteredOutputWindowControl()
         {
             this.InitializeComponent();
-
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                var filter = new Models.StringFilterContainer { Name = "Filter1", Id = Guid.NewGuid() };
+                this.DataContext = new FilteredOutputWindowViewModel { Filters = new System.Collections.ObjectModel.ObservableCollection<Models.StringFilterContainer> { filter }, EditingFilter = filter };
+            }
             Output.SizeChanged += (s, e) =>
             {
                 if (Scroller.Tag is bool autoScroll && autoScroll)
